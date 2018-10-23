1a65ecc9017c40ce9cbd463c172185305c8c8968
Alan-Miller <alan.ashman@gmail.com>
2018-10-22 20:12:17 -0600

    unused variables, image file, vulnerability, format, readme

--- src/images.js
+++ src/images.js
@@ -1,3 +1,21 @@
-module.exports = {
-  fall1: 
-}
+import fall1 from './imgs/fall1.jpg';
+import fall2 from './imgs/fall2.jpg';
+import fall3 from './imgs/fall3.jpg';
+import fall4 from './imgs/fall4.jpg';
+import fall5 from './imgs/fall5.jpg';
+import fall6 from './imgs/fall6.jpg';
+import fall7 from './imgs/fall7.jpg';
+import fall8 from './imgs/fall8.jpg';
+import fall9 from './imgs/fall9.jpg';
+
+export default [
+  fall1,
+  fall2,
+  fall3,
+  fall4,
+  fall5,
+  fall6,
+  fall7,
+  fall8,
+  fall9
+]
